8fea4f18b8846885fc043458fb0dcc5da0226c64
omar atef <change0m0@gmail.com>
2024-04-24 18:43:38 +0200

    one more lovely Js Challenge Completed

--- newChallenge 2/script.js
+++ newChallenge 2/script.js
@@ -16,3 +16,13 @@ const checkDogs = function (dogJulia, dogKate) {
   });
 };
 checkDogs(julia, kate);
+
+// next Challenge to calculate dogs ages to humanAge
+const calcAverageHumanAge = function (ages) {
+  const humanAge = ages.map(age => (age <= 2 ? age * 2 : age * 4 + 16));
+  return humanAge
+    .filter(year => year > 18)
+    .reduce((acc, year, i, arr) => acc + year / arr.length, 0);
+};
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
